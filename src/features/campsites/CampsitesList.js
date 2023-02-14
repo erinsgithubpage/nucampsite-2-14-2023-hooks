a8bf7915d7a3e30bef76062ab414e759a168c865
erinh <erin.hazelwood@outlook.com>
2023-02-14 07:12:17 -0500

    Hooks, 2: 11:15

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -9,7 +9,11 @@ const CampsitesList = ({ setCampsiteId }) => {
        
        {campsites.map((campsite) => {
             return (
-                <Col md='5' className='m-4' key={campsite.id}>
+                <Col 
+                md='5' 
+                className='m-4' 
+                key={campsite.id}
+                onClick={() => setCampsiteId() }>
                     <CampsiteCard campsite={campsite} />
                 </Col>
                     );
