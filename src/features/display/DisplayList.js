bc7aacf1f2d727348c27342d7dec291437e93720
erinh <erin.hazelwood@outlook.com>
2023-02-14 12:20:26 -0500

    SAPs router, five - 9:09

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -4,7 +4,19 @@ import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 
 const DisplayLit = () => {
-    const items = [selectFeaturedCampsites]
+    const items = [selectFeaturedCampsites(), selectFeaturedPromotion()]
+
+    return (
+        <Row>
+            {items.map((item, idx) => {
+                return (
+                    <Col md className='m-1' key={idx}>
+                        <DisplayCard item={item} />
+                    </Col>
+                )
+            } )}
+        </Row>
+    )
 };
 
 export default DisplayList;
