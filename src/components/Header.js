e5a4ed8a421010b743011d97a0b057f4ec9e2f0e
erinh <erin.hazelwood@outlook.com>
2023-02-14 08:30:08 -0500

    React Router, four- 3:54

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,44 @@ import {
     NavbarBrand,
     Collapse,
     NavbarToggler,
+    Nav,
     NavItem
   } from 'reactstrap';
   import { NavLink } from 'react-router-dom';
 import NucampLogo from '../app/assets/img/logo.png';
 
-
-function Header (props) {
-    const name = "";
+const Header = () => {
     return (
         <Navbar color='primary' sticky='top' expand='md'>
-        
           <NavbarBrand href='/'>
             <img src = {NucampLogo} alt='nucamp logo' />
           </NavbarBrand>
+          <Nav className='ms-auto' navbar>\
+            <NavItem>
+              <NavLink className='nav-link' to='/'>
+
+
+              </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink className='nav-link' to='/directory'>
+
+
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className='nav-link' to='/about'>
+
+
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className='nav-link' to='/contact'>
+
+
+                </NavLink>
+              </NavItem>
+          </Nav>
          
          
 
