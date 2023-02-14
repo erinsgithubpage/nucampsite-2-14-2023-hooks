109d78a3cdb5b229469246813a99f7c84661b997
erinh <erin.hazelwood@outlook.com>
2023-02-14 12:16:49 -0500

    SAPs router, five - 7:10

---
+++ src/features/display/DisplayList.js
@@ -0,0 +1,10 @@
+import { Col, Row } from 'reactstrap';
+import DisplayCard from './DisplayCard';
+import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
+import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
+
+const DisplayLit = () => {
+    const items = [selectFeaturedCampsites]
+};
+
+export default DisplayList;
