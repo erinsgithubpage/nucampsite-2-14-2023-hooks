7d4bf4eb68207efd7a55cb8b58b0dc55cef292c2
erinh <erin.hazelwood@outlook.com>
2023-02-14 08:38:00 -0500

    React Router, four - 5:57

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,31 +14,28 @@ const Header = () => {
     return (
         <Navbar color='primary' sticky='top' expand='md'>
           <NavbarBrand href='/'>
-            <img src = {NucampLogo} alt='nucamp logo' />
+            <img src = {NucampLogo} alt='nucamp logo' className='float-start'/>
+            <h1>NuCamp</h1>
           </NavbarBrand>
           <Nav className='ms-auto' navbar>\
             <NavItem>
               <NavLink className='nav-link' to='/'>
-
-
+                  <i className='fa fa-home fa-lg' /> Home
               </NavLink>
             </NavItem>
             <NavItem>
                 <NavLink className='nav-link' to='/directory'>
-
-
+                  <i className='fa fa-list fa-lg' /> Directory
                 </NavLink>
               </NavItem>
               <NavItem>
                 <NavLink className='nav-link' to='/about'>
-
-
+                  <i className='fa fa-info fa-lg' /> About
                 </NavLink>
               </NavItem>
               <NavItem>
                 <NavLink className='nav-link' to='/contact'>
-
-
+                  <i className='fa fa-address-card fa-lg' /> Contact
                 </NavLink>
               </NavItem>
           </Nav>
